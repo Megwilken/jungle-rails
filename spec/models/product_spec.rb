7a802cb43099bd7226198b5aedff2ab068855aef
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-27 15:00:37 -0600

    define quantity validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe Product, type: :model do
       expect(product).to_not be_valid
       expect(product.errors[:price]).to include("can't be blank")
     end
+    it 'validates presence of quantity' do
+      product = Product.new(quantity: nil)
+      expect(product).to_not be_valid
+      expect(product.errors[:quantity]).to include("can't be blank")
+    end
 end
