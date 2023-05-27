dc6a8667b056ad63067aa8cddf18332b24a5694b
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-27 15:15:56 -0600

    encountered bug, rremoved product spec code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,19 +7,4 @@ RSpec.describe Product, type: :model do
       expect(product).to_not be_valid
       expect(product.errors[:name]).to include("can't be blank")
     end
-    it 'validates presence of price' do
-      product = Product.new(price: nil)
-      expect(product).to_not be_valid
-      expect(product.errors[:price]).to include("can't be blank")
-    end
-    it 'validates presence of quantity' do
-      product = Product.new(quantity: nil)
-      expect(product).to_not be_valid
-      expect(product.errors[:quantity]).to include("can't be blank")
-    end
-    it 'validates presence of category' do
-      product = Product.new(category: nil)
-      expect(product).to_not be_valid
-      expect(product.errors[:category]).to include("can't be blank")
-    end
 end
