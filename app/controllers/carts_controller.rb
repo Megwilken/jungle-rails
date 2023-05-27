ac0e3ba559b451a72affb427d03cbb756592c414
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-27 15:35:39 -0600

    fix bug on My Cart when user not logged in

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,11 @@
 class CartsController < ApplicationController
-  before_filter :authorize
+  before_action :authorize
 
   def show
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def add_item
