b9e2c14201efa5a26daeab038950b406217dd244
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-27 16:04:36 -0600

    add validation for user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'Validations' do
+  it 'is valid with valid attributes' do
+    user = User.new(
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      first_name: 'John',
+      last_name: 'Doe'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a password' do
+    user = User.new(
+      email: 'test@example.com',
+      password: nil,
+      password_confirmation: 'password',
+      first_name: 'John',
+      last_name: 'Doe'
+    )
+    expect(user).to_not be_valid
+    expect(user.errors.full_messages).to include("Password can't be blank")
+  endadd
+
+end
 end
